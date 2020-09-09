14543e75d40218ddee4a03f8ea3e2d119fb5bee4
arban bani <arban89@hotmail.it>
2020-09-09 16:41:49 +0200

    Partially Completed Banking Microservice

--- MicroRabbit/MicroRabbit.Banking.Api/Startup.cs
+++ MicroRabbit/MicroRabbit.Banking.Api/Startup.cs
@@ -2,15 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Infra.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
+using Microsoft.OpenApi.Models;
+using MediatR;
 namespace MicroRabbit.Banking.Api
 {
     public class Startup
@@ -25,7 +29,27 @@ namespace MicroRabbit.Banking.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<BankingDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("BankingDbConnection"));
+            });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Banking MicroServices", Version = "v1" });
+            });
+
+            services.AddMediatR(typeof(Startup));
+            RegisterServices(services);
+
+
             services.AddControllers();
+
+        }
+
+        private void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +62,13 @@ namespace MicroRabbit.Banking.Api
 
             app.UseHttpsRedirection();
 
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Banking Microservices V1");
+            });
+
             app.UseRouting();
 
             app.UseAuthorization();
@@ -46,6 +77,7 @@ namespace MicroRabbit.Banking.Api
             {
                 endpoints.MapControllers();
             });
+
         }
     }
 }
